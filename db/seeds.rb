134817b9b0e25a3d37d01a6da9c08fcf480fa60f
Nicole De La Rosa <nicoledelarosa616@gmail.com>
2015-04-25 15:12:22 -0400

    Completed Topics and posts checkpoint. Added topics and nested posts within. Also, pushed a fix for the resolved checkbox for the Questions Edit view.

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,21 @@ require 'faker'
 end
 users = User.all
 
+# Create Topics
+15.times do
+  Topic.create!(
+    name: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph
+    )
+end
+topics = Topic.all
+
  
 # Create Posts
 50.times do
   Post.create!(
     user: users.sample,
+    topic: topics.sample,
     title:  Faker::Lorem.sentence,
     body:   Faker::Lorem.paragraph
   )
@@ -41,16 +51,35 @@ posts = Post.all
 end
 questions = Question.all
 
-user = User.first
-user.skip_reconfirmation!
-user.update_attributes!(
-  email: 'nicoledelarosa616@gmail.com',
-  password: 'monkey324'
+admin = User.new(
+  name: 'Admin User',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  role: 'admin'
   )
+admin.skip_confirmation!
+admin.save!
 
+moderator = User.new(
+  name: 'Moderator User',
+  email: 'moderator@example.com',
+  password: 'helloworld',
+  role: 'moderator'
+  )
+moderator.skip_confirmation!
+moderator.save!
+
+member = User.new(
+  name: 'Member User',
+  email:'member@example.com',
+  password: 'helloworld'
+  )
+member.skip_confirmation!
+member.save!
  
 puts "Seed finished"
 puts "#{User.count} users created"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
 puts "#{Question.count} questions created"
