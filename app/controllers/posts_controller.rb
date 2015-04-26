9b9e21361e3ea12ff72e924d8ad17cef5cd406e9
Nicole De La Rosa <nicoledelarosa616@gmail.com>
2015-04-26 00:06:51 -0400

    completed the validating posts checkpoint, which checks the post to make sure all of the components are included before saving to the database.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,13 +17,13 @@ class PostsController < ApplicationController
 
   def create
     @topic = Topic.find(params[:topic_id])
-    @post = current_user.posts.build(params.require(post_params))
+    @post = current_user.posts.build(post_params)
     authorize @post
     if @post.save
       flash[:notice] = "Post was saved."
       redirect_to [@topic, @post]
     else
-      flash[:error] = "There was an error savinig this post. Please try again."
+      flash[:error] = "There was an error saving this post. Please try again."
       render :new
     end
   end
@@ -38,7 +38,7 @@ class PostsController < ApplicationController
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize @post
-    if @post.update_attributes(params.require(post_params))
+    if @post.update_attributes(post_params)
       flash[:notice] = "Post was updated."
       redirect_to [@topic, @post]
     else
