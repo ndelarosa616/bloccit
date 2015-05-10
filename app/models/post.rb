222ffbc255bc94d798514a7c7bffea9318af89f8
Nicole De La Rosa <nicoledelarosa616@gmail.com>
2015-05-10 11:40:37 -0400

    removing after_create method from this branch

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,8 +13,6 @@ class Post < ActiveRecord::Base
   #validates :topic, presence: true
   #validates :user, presence: true
 
-  after_create :create_vote
-
   def up_votes
     votes.where(value: 1).count
   end
@@ -33,10 +31,3 @@ class Post < ActiveRecord::Base
 
     update_attribute(:rank, new_rank)
   end
-
-  private
-
-  def create_vote
-    user.votes.create(value: 1, post: self)
-  end
-end
