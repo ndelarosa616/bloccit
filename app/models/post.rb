cd42156dff59104e7db8144b6cbffed035fbcee1
Nicole De La Rosa <nicoledelarosa616@gmail.com>
2015-05-10 11:42:25 -0400

    Comple voting assignment. Added after_create method that automatically upvotes the post by the current user upon creation

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,12 +4,16 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :topic
 
+  mount_uploader :image, ImageUploader
+
   default_scope { order('rank DESC') }
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   #validates :topic, presence: true
   #validates :user, presence: true
+
+  after_create :create_vote
 
   def up_votes
     votes.where(value: 1).count
@@ -28,5 +32,11 @@ class Post < ActiveRecord::Base
     new_rank = points + age_in_days
 
     update_attribute(:rank, new_rank)
+  end
+
+  private
+
+  def create_vote
+    user.votes.create(value: 1, post: self)
   end
 end
