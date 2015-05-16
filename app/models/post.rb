723f98522d3a5e4977e4e170044b40705c808588
Nicole De La Rosa <nicoledelarosa616@gmail.com>
2015-05-16 09:28:10 -0400

    Finished mocking RSPEC assignment.

--- app/models/post.rb
+++ app/models/post.rb
@@ -36,4 +36,11 @@ class Post < ActiveRecord::Base
   def create_vote
     user.votes.create(value: 1, post: self)
   end
+
+  def save_with_initial_vote
+    ActiveRecord::Base.transaction do
+      @post = self
+      user.votes.create(value: 1, post: @post)
+    end
+  end
 end
