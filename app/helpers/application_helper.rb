4413050eac5d14321d93bf936b33cf84aa239243
Nicole De La Rosa <nicoledelarosa616@gmail.com>
2015-05-16 22:05:05 -0400

    completed another interlude assignment. Committing to master.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,18 @@ module ApplicationHelper
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render markdown).html_safe
   end
+
+  def up_vote_link_classes(post)
+    arrow = "glyphicon glyphicon-chevron-up "
+    vote = current_user.voted(post)
+    arrow += "voted" if vote && vote.up_vote?
+    arrow
+  end
+
+  def down_vote_link_classes(post)
+    arrow = "glyphicon glyphicon-chevron-down "
+    vote = current_user.voted(post)
+    arrow += "voted" if vote && vote.up_vote?
+    arrow
+  end
 end
